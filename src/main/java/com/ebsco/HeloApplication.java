c88962e6d5d51d4e56437652d5fd0c33df18985b
cmarch <cmarch@LP10-5CG517482Z.corp.epnet.com>
2016-11-10 17:53:51 -0500

    Changed args to argv

--- src/main/java/com/ebsco/HeloApplication.java
+++ src/main/java/com/ebsco/HeloApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HeloApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HeloApplication.class, args);
+	public static void main(String[] argv) {
+		SpringApplication.run(HeloApplication.class, argv);
 	}
 }
